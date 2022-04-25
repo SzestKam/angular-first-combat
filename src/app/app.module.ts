fb687ba0f4b5f6077faee6198a1faaa2d0ebcb5e
Kamil <dunno@gmail.com>
2022-04-25 18:41:33 +0200

    routing, bootstrap,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { FirstLessonComponent } from './first-lesson/first-lesson.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { ErrorComponent } from './error/error.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstLessonComponent
+    FirstLessonComponent,
+    HomePageComponent,
+    ErrorComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [FirstLessonComponent]
+  bootstrap: [HomePageComponent]
 })
 export class AppModule { }
