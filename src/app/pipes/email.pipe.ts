1b3ddedcd53ddfba0d3810e06aa8c1622d3cadc5
Kamil <dunno@gmail.com>
2022-04-27 17:39:07 +0200

    email pipe FIX

--- src/app/pipes/email.pipe.ts
+++ src/app/pipes/email.pipe.ts
@@ -7,8 +7,8 @@ export class EmailPipe implements PipeTransform {
 
   transform(value: string | undefined): string {
     if(value){
-      // return `<a href="mailto:${{value}}">${{value}}</a>`;
-      return value;
+      return `<a href="mailto:${value}">${value}</a>`;
+      //return value;
     }
     return '-';
   }
