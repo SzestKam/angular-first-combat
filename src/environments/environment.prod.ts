ab1a1f2361ee1cad5cf6f1488e9400fa6c5e5f3e
Kamil <dunno@gmail.com>
2022-04-28 21:04:06 +0200

    Chuck Noris integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://angular-f9286-default-rtdb.europe-west1.firebasedatabase.app/students'
+  backendUrl: 'https://angular-f9286-default-rtdb.europe-west1.firebasedatabase.app/students',
+  joke: {
+    randomUrl: 'https://api.chucknorris.io/jokes/random',
+    categoriesUrl: 'https://api.chucknorris.io/jokes/categories',
+    searchUrl: 'https://api.chucknorris.io/jokes/search'
+  }
 };
